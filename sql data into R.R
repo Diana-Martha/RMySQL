dc4488be31893581d56fa400ff0bd7d6504c925e
Diana-Martha <143635028+Diana-Martha@users.noreply.github.com>
2024-01-10 10:38:16 +0300

    Update sql data into R.R

--- sql data into R.R
+++ sql data into R.R
@@ -1,14 +1,15 @@
+#load necessary files
 library(DBI)
 library(RMySQL)
 
 #define id,password,host and database name created during database installation in MySQL
 USER<-'root'
-PASSWORD<-'hAniwa7@gracie'
+PASSWORD<-'....'
 HOST<-'localhost'
 DBNAME<-'world'
 
 
 db<-dbConnect(MySQL(),user=USER,password=PASSWORD,host=HOST,dbname=DBNAME,port=3306)
-result<-dbGetQuery(db,statement="select * from world.bangortelco_customers")
+result<-dbGetQuery(db,statement="select * from world.telco_customers")
 dbDisconnect(db)
 head(result)
